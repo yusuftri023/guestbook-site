050db5b071b7330e68dd31debd15f5cc6080f08e
root <yusuftri023@gmail.com>
2025-10-23 21:58:35 +0800

    qr size

--- lib/generate-qr.ts
+++ lib/generate-qr.ts
@@ -2,6 +2,6 @@ import qr from "qrcode";
 export async function generateQR(str: string) {
   return await qr.toDataURL(str, {
     errorCorrectionLevel: "H",
-    width: 80,
+    width: 90,
   });
 }
